8c5da573f0567226231125ce8248b2960287481d
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-03-13 16:53:54 +1100

    views not redirecting to dashboard on login or sign up - changing files to follow other examples - still not working

--- seeds/userSeeds.js
+++ seeds/userSeeds.js
@@ -3,22 +3,18 @@ const { User } = require("../models");
 const userData = [
   {
     username: "evan",
-    email: "test1@test.com",
     password: "password1",
   },
   {
     username: "ryan",
-    email: "test2@test.com",
     password: "password2",
   },
   {
     username: "charlotte",
-    email: "test3@test.com",
     password: "password3",
   },
   {
     username: "trevor",
-    email: "test4@test.com",
     password: "password4",
   },
 ];
